0e795e34b0a73204c15b64f7613f52ad2603a498
Hiroyuki Suminoue <suminoue.hiro@jp.fujitsu.com>
2017-07-14 17:15:05 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  // heroes: Hero[];
+  // selectedHero: Hero;
+
+  // constructor(private heroService: HeroService) { }
+
+  // ngOnInit(): void {
+  //   this.getHeroes();
+  // }
+
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
+
+  // getHeroes(): void {
+  //   this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  // }
 }
+
